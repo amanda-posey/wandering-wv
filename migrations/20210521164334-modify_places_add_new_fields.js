62829ab7ab1e9a322081efd6c29671c7901ff328
amanda=posey <amanda.posey@live.com>
2021-05-21 14:22:03 -0400

    seeding heroku dbs

--- migrations/20210521164334-modify_places_add_new_fields.js
+++ migrations/20210521164334-modify_places_add_new_fields.js
@@ -4,14 +4,6 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return Promise.all([
       queryInterface.addColumn(
-        'Users', // table name
-        'twitter', // new field name
-        {
-          type: Sequelize.STRING,
-          allowNull: true,
-        },
-      ),
-      queryInterface.addColumn(
         'places',
         'searchString',
         {
